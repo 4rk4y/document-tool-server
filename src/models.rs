da13f7184d8b8ae8213bcbb858d3cba90379766a
Lukas Widmer <luky.widmer@gmail.com>
2021-10-20 12:31:09 +0900

    added elements table

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,4 @@
+use super::schema::elements;
 use super::schema::pages;
 use serde::Serialize;
 
@@ -6,4 +7,20 @@ use serde::Serialize;
 pub struct Page {
     pub id: Option<i32>,
     pub title: String,
+}
+
+#[derive(Queryable, Insertable, Serialize)]
+#[table_name = "elements"]
+pub struct Element {
+    pub id: Option<i32>,
+    pub page_id: i32,
+    width: f32,
+    height: f32,
+    top: f32,
+    right: f32,
+    bottom: f32,
+    left: f32,
+    align: i32,
+    data_type: i32,
+    data: String,
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,29 @@
 table! {
+    elements (id) {
+        id -> Nullable<Integer>,
+        page_id -> Integer,
+        top -> Float,
+        right -> Float,
+        bottom -> Float,
+        left -> Float,
+        width -> Float,
+        height -> Float,
+        align -> Integer,
+        data_type -> Integer,
+        data -> Text,
+    }
+}
+
+table! {
     pages (id) {
         id -> Nullable<Integer>,
         title -> Text,
     }
 }
+
+joinable!(elements -> pages (page_id));
+
+allow_tables_to_appear_in_same_query!(
+    elements,
+    pages,
+);
